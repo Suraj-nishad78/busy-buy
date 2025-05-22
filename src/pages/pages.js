4652831df2512e4dc5a5e0f02fae9ceb7a82c92f
Suraj-nishad78 <iamsuraj0737@gmail.com>
2025-05-22 12:29:02 +0530

    final

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,7 +1,7 @@
 //All Pages are imported here Exported
 import Home from "./Home";
 import Signin from "./Signin";
-import Signup from "./signup";
+import Signup from "./Signup";
 import Cart from "./Cart";
 import MyOrders from "./MyOrders";
 import PageNotFound from "./PageNotFound";
